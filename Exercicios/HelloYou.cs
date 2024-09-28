09cd546795b1de945c12a4630ae082a2d17d79c2
Vanilson Sousa <vanilsonped7@gmail.com>
2024-09-28 10:50:32 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicios/HelloYou.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicios;
+public class HelloYou
+{
+    private string Nome;
+    private string LastName;
+
+    public void SetFullName()
+    {
+        Console.WriteLine("Digite seu nome: ");
+        Nome = Console.ReadLine();
+
+        Console.WriteLine("Digite seu sobrenome: ");
+        LastName = Console.ReadLine();
+
+    }
+
+    public void GetFullName()
+    {
+        Console.WriteLine($"Olá, {Nome} {LastName}! Seja muito bem-vindo(a)!");
+    }
+}
---
+++ Exercicios/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicios;
+
+public class Program
+{
+    public static void Main()
+    {
+        HelloYou pessoa1 = new HelloYou();
+        pessoa1.SetFullName();
+        pessoa1.GetFullName();
+    }
+}
